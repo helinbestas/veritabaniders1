056a316ead3f3ff1f1c9537ab77e2d2a2d05d538
helinbestas <154514379+helinbestas@users.noreply.github.com>
2023-12-27 22:02:15 +0300

    Add files via upload

---
+++ dortislem/dortislem/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dortislem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Lütfen 1. Sayıyı Giriniz");
+            double s1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Lütfen 2. Sayıyı Giriniz:");
+            double s2 = Convert.ToDouble(Console.ReadLine());
+
+            double sonuc = s1 + s2;
+            double sonuc1 = s1 - s2;
+            double sonuc2 = s1 * s2;
+            double sonuc3 = s1 / s2;
+
+
+
+            //Console.WriteLine("İşlemin Sonucu:" + Convert.ToString(sonuc));
+            Console.WriteLine(" Toplama İşleminin Sonucu:" + sonuc.ToString());
+            Console.WriteLine("Çıkarma İşleminin Sonucu:" + sonuc1.ToString());
+            Console.WriteLine("Çarpma İşleminin Sonucu:" + sonuc2.ToString());
+            Console.WriteLine("Bölme İşleminin Sonucu:" + sonuc3.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ dortislem/dortislem/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("dortislem")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("dortislem")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("29a7b677-b66d-413b-9ac2-0d11f07f35f9")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
